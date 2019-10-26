0a75abe570f431114e089356b84f1ef7e89a17c8
Andrea S <acsprema@hotmail.com>
2019-10-26 13:59:39 -0300

    creacion de base de datos sin update

---
+++ MyVet.Web/Data/Entities/History.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyVet.Web.Data.Entities
+{
+    public class History
+    {
+        public int Id { get; set; }
+
+        public ServiceType ServiceType { get; set; }
+
+        [Display(Name = "Description*")]
+        [MaxLength(100, ErrorMessage = "The {0} field can not have more than {1} characters.")]
+        [Required(ErrorMessage = "The field {0} is mandatory.")]
+        public string Description { get; set; }
+
+        [Display(Name = "Date*")]
+        [Required(ErrorMessage = "The field {0} is mandatory.")]
+        [DataType(DataType.DateTime)]
+        [DisplayFormat(DataFormatString = "{0:yyyy/MM/dd HH:mm}", ApplyFormatInEditMode = true)]
+        // A las propiedades Date le vamos a tener que tener miedito porque se supone que es un 
+        //Date En la WEB por lo que la hora va aser diferente en funcion del pais donde estemos
+        //asiq c/vez q definamos unsa DATE le agregamos una DATELOCAL de lectura  arrastrando las mismas data anotations
+        public DateTime Date { get; set; }
+
+        public string Remarks { get; set; }
+
+        public Pet Pet { get; set; }
+
+        [Display(Name = "Date*")]
+        [DisplayFormat(DataFormatString = "{0:yyyy/MM/dd HH:mm}", ApplyFormatInEditMode = true)]
+        public DateTime DateLocal => Date.ToLocalTime();
+
+    }
+}
